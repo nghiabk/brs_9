2c0d33ad2338d8a8c2d975ecffaf43203a214a02
hung son <hungson91cb@gmail.com>
2015-03-23 14:16:35 +0700

    show book

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,37 @@
+class CommentsController < ApplicationController
+
+  def index
+    @review = Review.find params[:review_id]
+    respond_to do |format|
+      format.html
+      format.js
+    end  
+  end
+
+  def create
+    @comment = Comment.new comment_params
+    @comment.user = current_user      
+    if @comment.save
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    else
+      redirect_to @comment.book  
+    end    
+  end
+
+  def destroy
+    @comment = Comment.find params[:id]
+    @comment.destroy
+    respond_to do |format|
+      format.html
+      format.js
+    end  
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit :content, :book_id, :review_id
+  end  
+end
